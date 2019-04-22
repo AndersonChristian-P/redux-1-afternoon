bd5f76a2d23db5cebb0e66f4bee3b84f8017d4b6
Christian Anderson <ca96187@gmail.com>
2019-04-22 13:05:52 -0600

    installed redux and completed Step 1

--- src/store.js
+++ src/store.js
@@ -1,1 +1,15 @@
+import { createStore } from "redux"
 
+
+let initialState = {
+
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default createStore(reducer)
