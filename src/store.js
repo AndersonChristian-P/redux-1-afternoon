f32df603a82652fc57ababf56b1036eb335766b1
Christian Anderson <ca96187@gmail.com>
2019-04-22 13:17:34 -0600

    completed Step 2

--- src/store.js
+++ src/store.js
@@ -2,11 +2,19 @@ import { createStore } from "redux"
 
 
 let initialState = {
-
+  recipeName: "",
+  recipeCategory: ""
 }
+
+export const UPDATE_RECIPE_NAME = "UPDATE_RECIPE_NAME"
+export const UPDATE_RECIPE_CATEGORY = "UPDATE_RECIPE_CATEGORY"
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case UPDATE_RECIPE_NAME:
+      return { ...state, recipeName: action.payload }
+    case UPDATE_RECIPE_CATEGORY:
+      return { ...state, recipeCategory: action.payload }
     default:
       return state
   }
